d040fbb93da794d8498ba29ce0430d5771449f23
jungAcat <hclove61@gmail.com>
2021-04-02 23:59:47 +0900

    basic algorithm

--- BaekJoon/11054/src/com/company/Main.java
+++ BaekJoon/11054/src/com/company/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static int[] DP1;
     public static int[] DP2;
     public static int[] DPSum;
+    public static int maxSum = 0;
     public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    int N = Integer.parseInt(br.readLine());
@@ -18,7 +19,7 @@ public class Main {
         for(int i = 1; i < N; i++) {
             DP1[i] = 1;
             for(int j = i-1; j >= 0; j--) {
-                if(nums[j] < nums[i] && DP1[i]) {
+                if(nums[j] < nums[i] && DP1[i] <= DP1[j]) {
                     DP1[i] = DP1[j] + 1;
                 }
             }
@@ -27,13 +28,14 @@ public class Main {
         for(int i = N-1; i >= 0; i--) {
             DP2[i] = 1;
             for(int j = i+1; j < N; j++) {
-                if(nums[j] < nums[i]) {
+                if(nums[j] < nums[i] && DP2[i] <= DP2[j]) {
                     DP2[i] = DP2[j] + 1;
-                    break;
                 }
             }
         }
-
-        System.out.println(0);
+        for(int i = 0; i < N; i++) {
+            maxSum = Integer.max(maxSum, DP1[i] + DP2[i]);
+        }
+        System.out.println(maxSum - 1);
     }
 }
